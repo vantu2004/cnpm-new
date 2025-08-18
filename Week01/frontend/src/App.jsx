d3441786b403bae9fc23b02454e1e26e4c099f09
vantu <tulevan526@gmail.com>
2025-08-18 09:13:12 +0700

    first commit

---
+++ Week01/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Thông tin cá nhân</h1>
+      <p>Họ tên: Lê Văn Tú</p>
+      <p>MSSV: 22110454</p>
+      <p>Khoa: Công nghệ thông tin</p>
+    </div>
+  );
+}
+
+export default App;
